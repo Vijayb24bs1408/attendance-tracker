dd6bd812eeedf9c9ab1ab6f75fd41d486fa1dfe5
Vijayb24bs1408 <b24bs1408@iitj.ac.in>
2026-07-20 18:43:04 +0530

    Use Firebase env variable and ignore backend in frontend ESLint

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,6 +1,7 @@
+/* eslint-env node */
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./attendance-tracker-d5112-firebase-adminsdk-fbsvc-7e4c45f8d9.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
